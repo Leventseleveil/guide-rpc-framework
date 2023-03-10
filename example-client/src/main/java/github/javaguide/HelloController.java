320ae792501bf3ed359f71cad1b3afd57d7c9ce4
xun <986458940@qq.com>
2023-03-11 03:33:58 +0800

    做了一个git测试

--- example-client/src/main/java/github/javaguide/HelloController.java
+++ example-client/src/main/java/github/javaguide/HelloController.java
@@ -15,7 +15,9 @@ public class HelloController {
     public void test() throws InterruptedException {
         String hello = this.helloService.hello(new Hello("111", "222"));
         //如需使用 assert 断言，需要在 VM options 添加参数：-ea
+        // test
         assert "Hello description is 222".equals(hello);
+        System.out.println("test");
         Thread.sleep(12000);
         for (int i = 0; i < 10; i++) {
             System.out.println(helloService.hello(new Hello("111", "222")));
